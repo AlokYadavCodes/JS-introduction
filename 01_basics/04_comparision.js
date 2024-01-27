14d405445b297e308fc78f237679ee169301f6b5
AlokYadavCodes <alokkumaryadav.cse@gmail.com>
2024-01-28 00:47:04 +0530

    Added codes to comparision file

---
+++ 01_basics/04_comparision.js
@@ -0,0 +1,23 @@
+console.log(3>2)
+console.log(3<2)
+console.log(5==6)
+
+console.log(1==true)   //true
+console.log(3==true)   //false
+console.log(5!=false)   //true
+console.log(5!=true)   //true
+
+console.log("a"==1) //false
+
+
+console.log("35"==35)  //true
+console.log("35"===35)  //false
+
+
+== checks the values
+=== checks the values and their data types also
+
+
+console.log("alok"=="hitesh") //false
+console.log("alok"=="alok")   // true
+console.log("alokyadav"<"ahitesh")  //checks the letter order from starting. ex: d<e is true and a>b is false
