407dfad40a9fbbebdb0cb7ee93b4031ee15ecdc0
AlokYadavCodes <alokkumaryadav.cse@gmail.com>
2024-01-28 00:19:06 +0530

    added operations in conversionOperation and created comparision file

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -1,20 +1,38 @@
-console.log("Converting string to Number")
-let score="45a" //string
-console.log(typeof(score))
-let IntValueOfScore=Number(score)
-console.log(typeof(IntValueOfScore))
-console.log(IntValueOfScore)
+// console.log("Converting string to Number")    //"45"=> 45  
+// let score="45a" //string                      // 45a=> NaN (Not a Number)
+// console.log(typeof(score))
+// let IntValueOfScore=Number(score)
+// console.log(typeof(IntValueOfScore))
+// console.log(IntValueOfScore)
 
-console.log("Converting Null to Number")
-score=null //null
-console.log(typeof(score))
-IntValueOfScore=Number(score)
-console.log(typeof(IntValueOfScore))
-console.log(IntValueOfScore)
+// console.log("Converting Null to Number")
+// score=null //null
+// console.log(typeof(score))
+// IntValueOfScore=Number(score)
+// console.log(typeof(IntValueOfScore))
+// console.log(IntValueOfScore)
 
-console.log("Converting undefined to Number")
-let points  //undefined
-console.log(typeof(points))
-let IntValueOfpoints=Number(points)
-console.log(typeof(IntValueOfpoints))
-console.log(IntValueOfpoints)
+// console.log("Converting undefined to Number")
+// let points  //undefined
+// console.log(typeof(points))
+// let IntValueOfpoints=Number(points)
+// console.log(typeof(IntValueOfpoints))
+// console.log(IntValueOfpoints)
+
+// console.log("Converting boolean to Number")    // true=> 1
+// score=true //boolean                           // false=> 0
+// console.log(typeof(score))
+// IntValueOfScore=Number(score)
+// console.log(typeof(IntValueOfScore))
+// console.log(IntValueOfScore)
+
+// let aName=null
+// let booleanValue=Boolean(aName)
+// console.log(booleanValue)
+
+
+// *********************** Operations ***********************
+
+console.log(10%11)
+console.log(16/2*8)   //first divide then multiply
+console.log(8*2/16)   //first multiply then divide
