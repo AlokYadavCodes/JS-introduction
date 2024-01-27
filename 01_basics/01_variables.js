693c7aac003916e0ab28963236f4712e3c2007fd
AlokYadavCodes <alokkumaryadav.cse@gmail.com>
2024-01-27 16:02:16 +0530

    fix typo

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -3,7 +3,7 @@ console.log(userName)
 const userId=121 //change not allowed
 let userStatus="Approved";
 userType="New"
-// userId=3233    It's will give error
+// userId=3233    It will give error
 
 // for string: use ""   
 // for numbers: just write it
