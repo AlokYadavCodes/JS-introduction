57e6b6fcea7825e287471b1df54271585613abbf
AlokYadavCodes <alokkumaryadav.cse@gmail.com>
2024-01-27 15:54:31 +0530

    Added dataTypes files and edited variables file

--- 01_basics/02_dataTypes.js
+++ 01_basics/02_dataTypes.js
@@ -3,16 +3,16 @@
 
 "use strict";  // Treat all the below codes as per new version which is better at handling errors and there is some changes in the meaning of few semantics. The browser must be supporting the newer version otherwise all the code will be treated like older version of JS
 
-let hi="hello"   //string
+let hi="hello"   //1)string data type
 console.log(hi)
 console.log(typeof hi)
-hi=3             //numbers
+hi=3             //2)numbers data type
 console.log(hi)
 console.log(typeof hi)
-hi=true          //boolean
+hi=true          //3)boolean data type
 console.log(hi)
 console.log(typeof hi)
-hi=BigInt(3434343343);  //appends n to the end of the value
+hi=BigInt(3434343343); //4) BigInt --> appends n to the end of the value
 console.log(hi)
 
 // typeof operator
@@ -20,4 +20,14 @@ console.log(hi)
 console.log(typeof hi)
 
 console.log(typeof "Alok")
+
+let bye  //5) undefined data type
+console.log(bye)
+console.log(typeof bye)
+
+let data=null
+console.log(data)         //6) null data type
+console.log(typeof data)   //type of null is object
+
+
 
