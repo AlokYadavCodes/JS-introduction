3584f3708475ff4f78a75b4fa4bf36e52873eac9
Alok Yadav <alokkumaryadav.cse@gmail.com>
2024-01-27 13:31:55 +0530

    Added basics file

---
+++ 01_basics.js
@@ -0,0 +1,10 @@
+let userName="Ramesh Yadav"
+const userId=121
+let userStatus="Approved";
+userType="New"
+
+// for string: use ""   
+// for numbers: just write it
+// a variable can be declared using 1) let--> perferred    2) var--> never use {scope problem}  3) just write variable name --> not recommended
+
+console.table([userName,userId,userStatus,userType]);
